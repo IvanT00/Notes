56314aa0a128405a3be990949fc2d9f25eb5bd8e
IvanT00 <ivan.teplov.00@inbox.ru>
2026-02-09 23:46:42 +0700

    added list of notes

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import classes from './App.module.scss'
+import './styles/_globals.scss'
+
+const App = () => {
+    return (
+        <div className='container'>
+            <h1 className={classes.mainTitle}>Заметки</h1>
+            <div className={classes.list}>
+                <div className={classes.listCont}>
+                    <div className={classes.item}>
+                        <h3 className={classes.item__title}>Новая заметка!</h3>
+                        <p className={classes.item__subtitle}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt eos mollitia obcaecati quidem quis repellendus repudiandae sed ut voluptate?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cumque, earum laudantium magnam magni necessitatibus nisi odit placeat saepe voluptatem.</p>
+                    </div>
+                    <div className={classes.item}>
+                        <h3 className={classes.item__title}>Новая заметка!</h3>
+                        <p className={classes.item__subtitle}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt eos mollitia obcaecati quidem quis repellendus repudiandae sed ut voluptate?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cumque, earum laudantium magnam magni necessitatibus nisi odit placeat saepe voluptatem.</p>
+                    </div>
+                    <div className={classes.item}>
+                        <h3 className={classes.item__title}>Новая заметка!</h3>
+                        <p className={classes.item__subtitle}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt eos mollitia obcaecati quidem quis repellendus repudiandae sed ut voluptate?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cumque, earum laudantium magnam magni necessitatibus nisi odit placeat saepe voluptatem.</p>
+                    </div>
+                    <div className={classes.item}>
+                        <h3 className={classes.item__title}>Новая заметка!</h3>
+                        <p className={classes.item__subtitle}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt eos mollitia obcaecati quidem quis repellendus repudiandae sed ut voluptate?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cumque, earum laudantium magnam magni necessitatibus nisi odit placeat saepe voluptatem.</p>
+                    </div>
+                    <div className={classes.item}>
+                        <h3 className={classes.item__title}>Новая заметка!</h3>
+                        <p className={classes.item__subtitle}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt eos mollitia obcaecati quidem quis repellendus repudiandae sed ut voluptate?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cumque, earum laudantium magnam magni necessitatibus nisi odit placeat saepe voluptatem.</p>
+                    </div>
+                    <div className={classes.item}>
+                        <h3 className={classes.item__title}>Новая заметка!</h3>
+                        <p className={classes.item__subtitle}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt eos mollitia obcaecati quidem quis repellendus repudiandae sed ut voluptate?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cumque, earum laudantium magnam magni necessitatibus nisi odit placeat saepe voluptatem.</p>
+                    </div>
+                    <div className={classes.item}>
+                        <h3 className={classes.item__title}>Новая заметка!</h3>
+                        <p className={classes.item__subtitle}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt eos mollitia obcaecati quidem quis repellendus repudiandae sed ut voluptate?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cumque, earum laudantium magnam magni necessitatibus nisi odit placeat saepe voluptatem.</p>
+                    </div>
+                    <div className={classes.item}>
+                        <h3 className={classes.item__title}>Новая заметка!</h3>
+                        <p className={classes.item__subtitle}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt eos mollitia obcaecati quidem quis repellendus repudiandae sed ut voluptate?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cumque, earum laudantium magnam magni necessitatibus nisi odit placeat saepe voluptatem.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
